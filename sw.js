f39ddfe6d91996c656995690b9904ac4ee99899d
Aleksei Adzhem <iamdjem@gmail.com>
2026-03-25 09:06:41 +0100

    Fix mobile layout: single-line with smaller boxes, all items fit on one row

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tracker-v1774425140';
+const CACHE = 'tracker-v1774426001';
 const ASSETS = ['/kubecon-tracker/', '/kubecon-tracker/index.html'];
 
 self.addEventListener('install', e => {
